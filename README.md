6ba257b4850c1cddeed8bc14b1f89eef7268d680
gustavomfranzin <gusfranzin@gmail.com>
2024-06-11 19:20:50 -0300

    init project

---
+++ README.md
@@ -0,0 +1,1 @@
+## Cidade Alta RP (EMBLEMAS)
---
+++ config.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const MYSQL_DATABASE = process.env.MYSQL_DATABASE || '';
+export const MYSQL_USER = process.env.MYSQL_USER || '';
+export const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD || '';
+export const MYSQL_ROOT_PASSWORD = process.env.MYSQL_ROOT_PASSWORD || '';
+export const PMA_HOST = process.env.PMA_HOST || '';
+export const API_PORT = process.env.API_PORT || 7000;
+export const PMA_PORT = process.env.PMA_PORT || 3006;
