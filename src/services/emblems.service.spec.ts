46a0d23694832796013aa8e1b2e583ce78ec50f2
gustavomfranzin <gusfranzin@gmail.com>
2024-06-13 03:47:22 -0300

    tests: corrigidos os testes unitarios e removido teste unitario da controller

--- src/services/emblems.service.spec.ts
+++ src/services/emblems.service.spec.ts
@@ -1,13 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { EmblemsController } from './emblems.controller';
+import { EmblemsController } from '../controllers/emblems.controller';
 import { EmblemsService } from './emblems.service';
-import { EmblemsRepository } from './emblems.repository';
-import { EmblemsType } from './emblems.interface';
+import { EmblemsRepository } from '../repository/emblems.repository';
+import { AccountsRepository } from '../repository/accounts.repository';
 
 describe('EmblemsService', () => {
   let emblemsService: EmblemsService;
 
-  const mockEmblems: EmblemsType[] = [
+  const mockEmblems = [
     {
       id: 1,
       slug: 'cda',
@@ -21,9 +21,15 @@ describe('EmblemsService', () => {
       image:
         'https://cidadealtarp.com/imagens/challenge/cidade-alta-valley.png',
     },
+    {
+      id: 3,
+      slug: 'policia',
+      name: 'Policia do Cidade Alta',
+      image: 'https://cidadealtarp.com/imagens/challenge/policia.png',
+    },
   ];
 
-  const mockEmblem: EmblemsType[] = [
+  const mockEmblemBySlug = [
     {
       id: 1,
       slug: 'cda',
@@ -32,11 +38,23 @@ describe('EmblemsService', () => {
     },
   ];
 
+  const mockEmblemsByUserId = {
+    name: 'João Da Silva',
+    email: 'joao.silva@cidadealta.com',
+    capturedEmblems: mockEmblems,
+  };
+
   const mockEmblemsRepository = () => ({
     getEmblems: jest.fn().mockResolvedValue(mockEmblems),
-    getEmblemsBySlug: jest.fn().mockImplementation(() => {
-      return mockEmblem;
-    }),
+    countEmblems: jest.fn().mockResolvedValue(3),
+    getEmblemsBySlug: jest.fn().mockResolvedValue(mockEmblemBySlug),
+    getManyEmblemsBySlug: jest.fn().mockResolvedValue(mockEmblems),
+  });
+
+  const mockAccountRepository = () => ({
+    getEmblemsByUserId: jest.fn().mockResolvedValue(mockEmblemsByUserId),
+    getUserByIdAndSlug: jest.fn().mockResolvedValue(false),
+    updateEmblemForUser: jest.fn(),
   });
 
   beforeEach(async () => {
@@ -44,6 +62,7 @@ describe('EmblemsService', () => {
       controllers: [EmblemsController],
       providers: [
         EmblemsService,
+        { provide: AccountsRepository, useFactory: mockAccountRepository },
         { provide: EmblemsRepository, useFactory: mockEmblemsRepository },
       ],
     }).compile();
@@ -51,17 +70,39 @@ describe('EmblemsService', () => {
   });
 
   describe('/emblems', () => {
-    it('should return an array of object from emblems', async () => {
-      const result = await emblemsService.getEmblems();
-      expect(result).toBe(mockEmblems);
+    it('should return all emblems', async () => {
+      const data = {
+        page: 1,
+        itemsPerPage: 10,
+      };
+
+      const mockExpect = {
+        itemsPerPage: 10,
+        page: 1,
+        pages: 1,
+        totalItems: 3,
+        items: mockEmblems,
+      };
+
+      const result = await emblemsService.getEmblems(data);
+      expect(result).toStrictEqual(mockExpect);
     });
   });
 
   describe('/emblems/:slug', () => {
-    it('should return an array of object for slug "cda"', async () => {
+    it('should return emblem with slug "cda"', async () => {
+      const userId = 1;
       const slug = 'cda';
-      const result = await emblemsService.getEmblemsBySlug(slug);
-      expect(result).toBe(mockEmblem);
+      const result = await emblemsService.getEmblemsBySlug(userId, slug);
+      expect(result).toBe(mockEmblemBySlug);
+    });
+  });
+
+  describe('/:userId/emblems', () => {
+    it('should return emblems user', async () => {
+      const userId = 1;
+      const result = await emblemsService.getEmblemsByUserId(userId);
+      expect(result).toStrictEqual(mockEmblemsByUserId);
     });
   });
 });
